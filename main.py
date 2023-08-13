ba35d2d425ab57ca6272c5225fae27a9a83d889e
Snappu <92256743+Snappu@users.noreply.github.com>
2023-08-13 01:00:14 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk ()
+root.title("Fedorascript_GUI")
+root.geometry('800x600')
+root.resizable(width=False, height=False)
+
+def click_button():
+	os.system('chmod +x script_two.sh')
+	os.system('./script_two.sh')
+	root.destroy()
+	messagebox.showinfo('good', 'Script started')
+	termstop = input("Click enter to close...")
+
+lb = Label(root, text="Start", font=("Aria Bolt", 20))
+lb.pack(expand=True)
+
+btn = Button(root, text="Start", command=click_button)
+btn.pack(expand=True, ipadx=40, ipady=20)
+
+root.mainloop()
